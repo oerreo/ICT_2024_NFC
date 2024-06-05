6b32080f0372f33ede3dfa7d1f78af9f0f61c203
oerreo <rhxogns13@gmail.com>
2024-06-05 19:14:35 +0900

    update_addUser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const socketIo = require("socket.io");
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
 const mysql = require("mysql");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -37,6 +38,8 @@ const portSettings = {
 const port = new SerialPort(portSettings);
 const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
+let lastNfcData = null; // 가장 최근에 읽은 NFC 데이터를 저장할 변수
+
 // 시리얼 데이터 수신
 parser.on("data", (data) => {
   console.log(`Received data: ${data}`);
@@ -51,6 +54,8 @@ parser.on("data", (data) => {
   if (match) {
     nfcData = match[0].replace(/\s+/g, "");
     console.log(`Extracted nfc data: ${nfcData}`); // Extracted 데이터 로그
+    lastNfcData = nfcData; // 최근 NFC 데이터를 저장
+    io.emit("nfc_uid", nfcData); // 클라이언트로 UID 전송
   } else {
     console.log("No valid UID found in the data");
     return;
@@ -73,18 +78,47 @@ parser.on("data", (data) => {
       }
       if (results.length > 0) {
         const userName = results[0].user_name;
-        const stuNum = results[0].stu_num;
-        stuNum.replace();
+        const stuNum = String(results[0].stu_num); // stuNum을 문자열로 변환
         console.log("NFC matched: ", results[0]);
-        //찍혀있는 uid가 로그에 남으니까 추적가능.
         io.emit("NFC_Match", `출입자: ${userName} (${stuNum.substr(0, 7)})`);
-        //io.emit("NFC_Match", `NFC matched: ${results[0].uid}`);
       } else {
         console.log("NFC not allowed");
         io.emit("NFC_Match", "NFC not allowed");
       }
     }
   );
+});
+
+// 새로운 유저 데이터 수신 및 데이터베이스 저장
+io.on("connection", (socket) => {
+  socket.on("add_nfc_user", (userData) => {
+    const { userName, stuNum } = userData;
+    const uid = lastNfcData;
+
+    if (!uid) {
+      socket.emit("database_error", "NFC UID를 읽지 못했습니다.");
+      return;
+    }
+
+    db.query(
+      "INSERT INTO allow_uid (user_name, stu_num, uid) VALUES (?, ?, ?)",
+      [userName, stuNum, uid],
+      (err, results) => {
+        if (err) {
+          console.error("Error inserting into database: " + err.message);
+          socket.emit("database_error", "등록에 실패하였습니다.");
+          return;
+        }
+        console.log("User added to database:", results);
+        socket.emit("database_success", "성공적으로 등록되었습니다.");
+      }
+    );
+  });
+});
+
+// form.html 제공
+app.get("/form/form.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "form", "form.html"));
 });
 
 // 시리얼 포트 에러 처리
